373a8cbfa56fded9e1c34fb8d46dccaec687ec01
Filipe Leuch Bonfim <flb09@inf.ufpr.br>
2014-04-25 00:34:33 -0300

    Add optinal disciplines
    Signed-off-by: Filipe Leuch Bonfim <flb09@inf.ufpr.br>

--- ci320-tpc/trabalho_4_visualizeitor/js/visualizeitor.js
+++ ci320-tpc/trabalho_4_visualizeitor/js/visualizeitor.js
@@ -1,42 +1,82 @@
 
 $( document ).ready(function() {
-  
-    if (window.XMLHttpRequest)
-      {// code for IE7+, Firefox, Chrome, Opera, Safari
+    if (window.XMLHttpRequest) {// code for IE7+, Firefox, Chrome, Opera, Safari
       xmlhttp=new XMLHttpRequest();
-      }
-    else
-      {// code for IE6, IE5
+    }
+    else {// code for IE6, IE5
       xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-      }
+    }
     xmlhttp.open("GET","alunos.xml",false);
     xmlhttp.send();
     xmlDoc=xmlhttp.responseXML;
 
     var aluno = xmlDoc.getElementsByTagName("ALUNO");
     var btGrr = document.getElementById('btngrr').value;
+    var opt = 0;
 
-    setColor = function(cod,sig) {      
-      var disc = document.getElementById(cod);
-      if(disc) {
-        //alert("Foi color");
+    getDisciplineCode = function(alunoTag) {  
+      var desc_est = aluno[alunoTag].getElementsByTagName("DESCR_ESTRUTURA")[0].childNodes[0].nodeValue;   
+      var cod = aluno[alunoTag].getElementsByTagName("COD_ATIV_CURRIC")[0].childNodes[0].nodeValue;
+      
+      if(document.getElementById(cod)) {
+        return '#'+cod;
+      } else if(desc_est == 'Obrigatórias') {
+          // Disciplina é obrigatória no 
+          // xm, mas não está na grade atual, EX: CI066
+          // o contrário acontece com CI209
+        } else if(desc_est == 'Optativas') {
+          if(opt) {
+            var valueDisc;
+            for (var j = 1; j <= opt; j++) {
+              valueDisc = $('#OPT'+j).textContent;
+              // Se a disciplica ja está na lista de optativas    
+              if(valueDisc == cod) {
+                return '#OPT'+opt;  
+              }
+            }
+          }
+          
+          if(opt < 6){ // Nova optativa - Se não acabou os slots para optativas
+              opt++;
+              $('#OPT'+opt).html(cod);            
+              return '#OPT'+opt;
+          }        
+      }
+      return false;
+    }
+
+    setColor = function(cod,sig) { 
+      if(cod) {
         var bg = 'background-color';
         switch(sig) {
-          case "Aprovado":
-              $('#'+cod).css(bg,'#86C543');
+          case "Aprovado": // Green
+            $(cod).css(bg,'#86C543');
             break;
-          case "Reprovado":         
+          case "Reprovado": // Red 
           case "Repr. Freq":
-            $('#'+cod).css(bg,'#FF372E');
+            $(cod).css(bg,'#FF372E');
             break;
-          case "Equivale":
-            $('#'+cod).css(bg,'#E8E340');
+          case "Equivale": // Yellow
+            $(cod).css(bg,'#E8E340');
             break;
-          case "Matricula":
-            $('#'+cod).css(bg,'#484CF5');
+          case "Matricula": // Blue
+            $(cod).css(bg,'#595DF6');
             break;
-          default:
+          default: // White - não cursando
             break;
+        }
+      }
+    }
+
+    customizeDisciplines = function() { 
+      opt = 0;
+      $('.cod_disc').css('background-color','white');
+      var grr = document.getElementById('grr').value; 
+      for (var i=0;i<aluno.length;i++) {    
+        if(aluno[i].getElementsByTagName("MATR_ALUNO")[0].childNodes[0].nodeValue == grr) {
+          var sig = aluno[i].getElementsByTagName("SIGLA")[0].childNodes[0].nodeValue;
+          var cod = getDisciplineCode(i);
+          setColor(cod,sig);
         }
       }
     }
@@ -44,20 +84,14 @@ $( document ).ready(function() {
      /*
         EVENTS   
      */
-      
+      document.onkeydown = function(event) {
+         if (event.keyCode == 13) {
+            customizeDisciplines();
+         }
+      }
+
       $("#btngrr").click(function(){
-        $('.cod_disc').css('background-color','white');
-        var grr = document.getElementById('grr').value; 
-        //aprovado em verde, reprovado em vermelho, matriculado em azul, 
-        // equivalência em amarelo e não cursado em branco
-        for (i=0;i<aluno.length;i++) {    
-          if(aluno[i].getElementsByTagName("MATR_ALUNO")[0].childNodes[0].nodeValue == grr) {
-            var cod = aluno[i].getElementsByTagName("COD_ATIV_CURRIC")[0].childNodes[0].nodeValue;
-            var sig = aluno[i].getElementsByTagName("SIGLA")[0].childNodes[0].nodeValue;
-            //alert(cod+" i: "+i);
-            setColor(cod,sig);
-          }
-        }
+        customizeDisciplines();
       });
 });
 //GRR00000007
