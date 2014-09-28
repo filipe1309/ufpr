3cf1ac3407d67814bfce0f3e6bcd00670a766eff
Filipe Leuch Bonfim <flb09@inf.ufpr.br>
2014-09-28 14:36:10 -0300

    ci317: Add homework 6
    Signed-off-by: Filipe Leuch Bonfim <flb09@inf.ufpr.br>

---
+++ ci317/trabalho_1_6/Trabalho6_Fragments/app/src/main/java/com/example/filipe1309/trabalho6_fragments/Fragment_B.java
@@ -0,0 +1,17 @@
+package com.example.filipe1309.trabalho6_fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by filipe1309 on 26/09/14.
+ */
+public class Fragment_B extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_b, container, false);
+    }
+}
